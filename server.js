533b28375892c459677228c7580eed326b4e48b4
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-15 19:26:02 +0200

    add first endpoint

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express(); //twprzenie nowej aplikacji express i przypisanie do stąłej app
 
+app.get('/', (req, res) => { //app to odnośnik do serwera. get to metodą którą chcemy obsługiwać w endpoincie
+    res.send('<h1>My first server!</h1>'); //req=request(zapytanie o informacje o użytkowniku) res=respons(odpowiedzi zawiera metody do komunikacji zwrotnej)
+});
+
 app.listen(8000, () => {  //aplikacja app. utworzeni serwera http na porcie 8000
   console.log('Server is running on port: 8000');
 });

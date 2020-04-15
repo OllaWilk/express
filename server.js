ea5f88fa4d393cb462f6310eac19e5f25c1c28fb
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-15 20:55:44 +0200

    add subpages and paths to them

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const path = require('path'); //klei ścieżki
+
 
 const app = express(); //twprzenie nowej aplikacji express i przypisanie do stąłej app
 
 app.get('/', (req, res) => { //app to odnośnik do serwera. get to metodą którą chcemy obsługiwać w endpoincie
-    res.send('<h1>My first server!</h1>'); //req=request(zapytanie o informacje o użytkowniku) res=respons(odpowiedzi zawiera metody do komunikacji zwrotnej)
+    res.sendFile(path.join(__dirname + '/views/index.html')); //req=request(zapytanie o informacje o użytkowniku) res=respons(odpowiedzi zawiera metody do komunikacji zwrotnej)
 });
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/contact.html'));
+});
+
+app.get('/info', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/info.html'));
+});
+
+app.get('/history', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/history.html'));
+});
+
 
 app.listen(8000, () => {  //aplikacja app. utworzeni serwera http na porcie 8000
   console.log('Server is running on port: 8000');

359aa110fea5f4be995ebe61708674f2831442fb
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-16 17:01:25 +0200

    install expres-handlebars and use it to change endpoint in Hello page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
+
 
 app.use((req, res, next) => {
     res.show = (name) => {
@@ -11,15 +14,15 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname + '/style.css'));
-  });
-
-app.use(express.static(path.join(__dirname + '/img')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 
 app.get('/', (req, res) => {
     res.show('index.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
@@ -37,6 +40,7 @@ app.get('/info', (req, res) => {
 app.get('/history', (req, res) => {
     res.show('history.html');
 });
+
 
 
 app.use((req, res) => {

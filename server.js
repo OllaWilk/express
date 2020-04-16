eafa693270b355228e42b0360977ecb28e27a525
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-16 17:17:11 +0200

    use hbs in all project

--- server.js
+++ server.js
@@ -7,41 +7,32 @@ app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname + `/views/${name}`));
-    };
-    next();
-});
-
 app.use(express.static(path.join(__dirname + '/public')));
 
 
 app.get('/', (req, res) => {
-    res.show('index.html');
+    res.render('index', { layout: false });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { layout: false });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { layout: false });
+});
+
+app.get('/info', (req, res) => {
+    res.render('info', { layout: false });
+});
+
+app.get('/history', (req, res) => {
+    res.render('history', { layout: false });
 });
 
 app.get('/hello/:name', (req, res) => {
     res.render('hello', { layout: false, name: req.params.name });
 });
-
-app.get('/about', (req, res) => {
-    res.show('about.html');
-});
-
-app.get('/contact', (req, res) => {
-    res.show('contact.html');
-});
-
-app.get('/info', (req, res) => {
-    res.show('info.html');
-});
-
-app.get('/history', (req, res) => {
-    res.show('history.html');
-});
-
-
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
